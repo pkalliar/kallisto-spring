79aadde797b923b87a16eb705abdf89eff509b64
Panagiotis Kalliaras <pkalliar@yahoo.com>
2018-01-17 11:16:18 +0200

    commit everything under new name

---
+++ src/main/java/com/pankal/Application.java
@@ -0,0 +1,22 @@
+package com.pankal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableScheduling
+@SpringBootApplication
+public class Application {
+
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
