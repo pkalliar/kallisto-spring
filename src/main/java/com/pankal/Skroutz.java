e8b1b57049cadc076d25a0a3d370dac58ab70233
Panagiotis Kalliaras <pkalliar@yahoo.com>
2018-02-05 09:14:13 +0200

    new skroutz controller

--- src/main/java/com/pankal/Skroutz.java
+++ src/main/java/com/pankal/Skroutz.java
@@ -6,7 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
@@ -19,29 +21,26 @@ public class Skroutz {
 
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-
-	public static void doit() {
-		RestTemplate restTemplate = new RestTemplate();
-
+	private static HttpEntity<?> prepareRequest(){
 
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
 		headers.add("Accept", "application/vnd.skroutz+json; version=3");
-		headers.add("Authorization", "Bearer κκκo6h9k-txp-YKy7-rIMTJmcVPad16a-aS6C_pmySghriSN4ne6boaKTr4ngUkt7Y-GhCy_iz7DPfXjX7obDMdfA==");
-//
+		headers.add("Authorization", "Bearer o6h9k-txp-YKy7-rIMTJmcVPad16a-aS6C_pmySghriSN4ne6boaKTr4ngUkt7Y-GhCy_iz7DPfXjX7obDMdfA==");
+
+		return new HttpEntity<Object>(headers);
+	}
 
 
-		String url1 = "https://api.skroutz.gr/api/search";
-		String url2 = "http://api.worldbank.org/v2/en/sources/15/series/all/metadata?page=1&per_page=20000&format=json";
+	public static JsonNode search(String toSearch) {
+		RestTemplate restTemplate = new RestTemplate();
 
-//		HttpEntity<String> entity = new HttpEntity<String>(postBodyJson ,headers);
-//		restTemplate.get(url1, entity);
+		String url1 = "https://api.skroutz.gr/api/search?q=" + toSearch;
 
-		MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
-		params.set("Accept", "application/vnd.skroutz+json; version=3");
-		params.set("Authorization", "Bearer κκκo6h9k-txp-YKy7-rIMTJmcVPad16a-aS6C_pmySghriSN4ne6boaKTr4ngUkt7Y-GhCy_iz7DPfXjX7obDMdfA==");
+		ResponseEntity<JsonNode> quote =
+				restTemplate.exchange(url1, HttpMethod.GET, prepareRequest(), JsonNode.class);
 
-		JsonNode quote = restTemplate.getForObject(url2, JsonNode.class, params);
-		log.info(quote.toString());
+//		log.info("quote is " + quote.getBody());
+		return quote.getBody();
 	}
 }
---
+++ src/main/java/com/pankal/shop/skroutz/SkroutzController.java
@@ -0,0 +1,39 @@
+package com.pankal.shop.skroutz;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.pankal.priority.Priority;
+import com.pankal.priority.PriorityRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/skroutz")
+@CrossOrigin("*")
+public class SkroutzController {
+
+	private PriorityRepository priorityRepository;
+
+	private static final Logger log = LoggerFactory.getLogger(SkroutzController.class);
+
+	public SkroutzController(PriorityRepository priorityRepository) {
+		this.priorityRepository = priorityRepository;
+	}
+
+
+
+	@GetMapping("/search/{toSearch}")
+	public JsonNode search(@PathVariable String toSearch) {
+		log.info("in getContact with id " + toSearch);
+		return JsonNodeFactory.instance.objectNode().put("result", "to-do skroutz search");
+	}
+
+}
