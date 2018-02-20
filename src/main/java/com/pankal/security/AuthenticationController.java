54703bb9e3b791f2975d70559991b19601962eee
Panagiotis Kalliaras <pkalliar@yahoo.com>
2018-02-21 00:21:47 +0200

    home

--- src/main/java/com/pankal/security/AuthenticationController.java
+++ src/main/java/com/pankal/security/AuthenticationController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @RestController
 @RequestMapping("/api/login")
